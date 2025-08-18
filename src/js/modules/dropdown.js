ca553557007d6d91df845f4e0042bb63494c72e4
Nero <s.kachalko@imedia.by>
2025-08-18 13:10:27 +0300

    update

--- src/js/modules/dropdown.js
+++ src/js/modules/dropdown.js
@@ -2,9 +2,12 @@ export const dropdown = () => {
     document.body.addEventListener('click', (e) => {
         const dropdown = e.target.closest('.js-dropdown');
         const allDropdowns = document.querySelectorAll('.js-dropdown.active');
+        const isOption = e.target.closest('.js-dropdown__option');
 
         if (dropdown) {
-            dropdown.classList.toggle('active');
+            if (!isOption) {
+                dropdown.classList.toggle('active');
+            }
 
             allDropdowns.forEach(d => {
                 if (d !== dropdown) d.classList.remove('active');
